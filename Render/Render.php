fcd261e060e511273daf70d04688167a2fd45de0
Breno Douglas <breninho94@gmail.com>
2015-03-12 19:41:30 -0300

    Add modules views

--- Render/Render.php
+++ Render/Render.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 namespace Render\Render;
 
 use Render\Render\Interfaces\InterfaceRender;
@@ -7,31 +7,31 @@ use Render\Render\Interfaces\InterfaceRender;
  * 
  * @author Breno Douglas <bdouglasans@gmail.com>
  */
-class Render implements InterfaceRender 
+class Render implements InterfaceRender
 {
-    
+
     private $scriptsCollection;
     private $stylesCollection;
     private $content;
     private $html;
     private static $viewPath;
     private static $layout;
-    
+
     public function __construct()
     {
         $this->scriptsCollection = new \ArrayIterator();
         $this->stylesCollection = new \ArrayIterator();
         //$this->registerHelpers();
     }
-    
+
     public function __call($name, $arguments)
     {
-        if(isset($this->$name)){
+        if (isset($this->$name)) {
             $object = clone $this->$name;
             return $object();
         }
     }
-    
+
     public function appendScript($path)
     {
         $this->scriptsCollection->append($path);
@@ -68,62 +68,66 @@ class Render implements InterfaceRender
 
         return $html;
     }
-	
-	public function renderTemplate($page, array $vars = array(), $layout = true)
-	{
-		$this->extractVar($vars);
+
+    public function renderTemplate($page, array $vars = array(), $layout = true)
+    {
+        $this->extractVar($vars);
         $this->createContent($page);
         
-        $fileLayout = self::$viewPath. "/" . self::$layout. ".phtml";
+        $page = explode(":", $page)[0];
+        
+        $fileLayout = self::$viewPath[$page] . "/" . self::$layout . ".phtml";
         if ($layout == true && file_exists($fileLayout)) {
             ob_start();
-                require_once $fileLayout;
+            require_once $fileLayout;
             $this->html = ob_get_clean();
             echo $this->html;
         } else {
             echo $this->content();
         }
-	}
-	
+    }
+
     public static function render($page, array $vars = array(), $layout = true)
-    {   
-		$render = new static();
+    {
+        $render = new static();
         $render->renderTemplate($page, $vars, $layout);
     }
-    
+
     private function extractVar(array $var)
     {
         foreach ($var as $key => $value):
             $this->$key = $value;
         endforeach;
     }
-    
+
     public function createContent($action)
-    {
-        $file = self::$viewPath . '/' . $action . '.phtml';
+    {   
+        $actions = explode(":", $action);
         
-        if(file_exists($file)) {
+        $file = self::$viewPath[$actions[0]] . '/' . $actions[1] . '.phtml';
+
+        if (file_exists($file)) {
             ob_start();
-                require_once $file;
+            require_once $file;
             $this->content = ob_get_clean();
         } else {
             throw new \Exception("View file no exists for action");
         }
     }
-    
-    public static function setLayoutPath($path) 
+
+    public static function setLayoutPath($alias, $path)
     {
-        self::$viewPath = $path;
+        self::$viewPath[ucfirst($alias)] = $path;
     }
-    
-    public static function setupLayout($layout) 
-    {
-        self::$layout = $layout;
-    }
-    
-    public function extendsLayout($layout) 
+
+    public static function setupLayout($layout)
     {
         self::$layout = $layout;
     }
-	
+
+    public function extendsLayout($layout)
+    {
+        self::$layout = $layout;
+    }
+
 }
